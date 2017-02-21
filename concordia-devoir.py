14e6565fad743030047ac3f747fa462f9119e148
campparent <cam.pparent@gmail.com>
2017-02-20 23:32:21 -0500

    Update concordia-devoir.py

--- concordia-devoir.py
+++ concordia-devoir.py
@@ -27,4 +27,10 @@ print(len(longTitre))
 #for phrase in fichier:
     #print(diplome("{} de ".format(fichier)),auteur("{} compte ".format(fichier)),nbPages("{} de pages. Son titre est ".format(fichier)),longTitre)
     #print(len(longTitre))
+
+#La formule n'est pas complète mais c'est le mieux que j'ai su faire avec l'aide de l'ami Google. Avec print(longTitre,diplome,nbPages) et
+#print(len(longTitre)), je n'obtiens que les variables qui ne sont pas en relation entre elles, comme elles l'auraient été dans la phrase
+#demandée. J'ai essayé de les mettre dans une phrase, mais celle-ci n'as pas fonctionné malgré mes différents essais. Bref.
+
+
     
